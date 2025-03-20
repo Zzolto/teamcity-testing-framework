19da838f70507f74b2496cb8b2e881ab67deff67
Zolto <zolto_gunzenov@mail.ru>
2025-03-20 18:23:00 +0300

    realize first buidConf test

--- src/main/java/com/example/teamcity/api/spec/Specification.java
+++ src/main/java/com/example/teamcity/api/spec/Specification.java
@@ -2,55 +2,37 @@ package com.example.teamcity.api.spec;
 
 import com.example.teamcity.api.config.Config;
 import com.example.teamcity.api.models.User;
-import io.restassured.authentication.AuthenticationScheme;
-import io.restassured.authentication.BasicAuthScheme;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.apache.http.client.methods.RequestBuilder;
-
-import java.util.List;
 
 public class Specification {
-    private static Specification spec;
 
-    private String url = Config.getProperties("host") + ":" +Config.getProperties("port");
-
-    private Specification(){
-
-    }
-
-    public static Specification getSpecification(){
-        if(spec == null)
-            spec = new Specification();
-        return spec;
-    }
-
-    private RequestSpecBuilder reqBuilder(){
-        RequestSpecBuilder requestBuilder = new RequestSpecBuilder();
-        requestBuilder.setBaseUri("http://" + url).build();
+    private static RequestSpecBuilder reqBuilder() {
+        var requestBuilder = new RequestSpecBuilder();
+        requestBuilder.addFilter(new RequestLoggingFilter());
+        requestBuilder.addFilter(new ResponseLoggingFilter());
         requestBuilder.setContentType(ContentType.JSON);
         requestBuilder.setAccept(ContentType.JSON);
-        requestBuilder.addFilters(List.of(new RequestLoggingFilter(), new ResponseLoggingFilter()));
-
         return requestBuilder;
     }
 
-    public RequestSpecification unauthSpec(){
-        return reqBuilder().build();
+    public static RequestSpecification superUserSpec() {
+        var requestBuilder = reqBuilder();
+        requestBuilder.setBaseUri("http://%s:%s@%s/httpAuth".formatted("", Config.getProperties("superUserToken"), Config.getProperties("host")));
+        return requestBuilder.build();
     }
 
-    public RequestSpecification authSpec(User user){
-        BasicAuthScheme basicAuth = new BasicAuthScheme();
-        basicAuth.setUserName(user.getUser());
-        basicAuth.setPassword(user.getPassword());
+    public static RequestSpecification unauthSpec() {
+        var requestBuilder = reqBuilder();
+        return requestBuilder.build();
+    }
 
-        return reqBuilder()
-                .setAuth(basicAuth)
-                .build();
+    public static RequestSpecification authSpec(User user) {
+        var requestBuilder = reqBuilder();
+        requestBuilder.setBaseUri("http://%s:%s@%s".formatted(user.getUsername(), user.getPassword(), Config.getProperties("host")));
+        return requestBuilder.build();
     }
 }
--- src/test/java/com/example/teamcity/api/BuildConfigurationTest.java
+++ src/test/java/com/example/teamcity/api/BuildConfigurationTest.java
@@ -1,16 +1,51 @@
 package com.example.teamcity.api;
 
+import com.example.teamcity.api.enums.Endpoint;
+import com.example.teamcity.api.models.BuildType;
+import com.example.teamcity.api.models.Project;
+import com.example.teamcity.api.models.User;
+import com.example.teamcity.api.requests.checked.CheckedBase;
+import com.example.teamcity.api.spec.Specification;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static com.example.teamcity.api.generators.TestDataGenerator.generate;
 import static io.qameta.allure.Allure.step;
 
 public class BuildConfigurationTest extends BaseApiTest{
 
     @Test(description = "User should be able to create build type", groups = {"Positive", "CRUD"})
     public void userCreatesBuildTypeTest(){
-        step("Create User");
-        step("Create project by user");
-        step("Create buildType for project by user");
-        step("Check buildTYpe was created successfully with correct data");
+        var user = generate(User.class);
+
+        step("Create User", () -> {
+            var requester = new CheckedBase<User>(Specification.superUserSpec(), Endpoint.USERS);
+            requester.create(user);
+        });
+
+        var project = generate(Project.class);
+        AtomicReference<String> projectId = new AtomicReference<>("");
+
+        step("Create project by user", () -> {
+            var requester = new CheckedBase<Project>(Specification.authSpec(user), Endpoint.PROJECTS);
+            projectId.set(requester.create(project).getId());
+        });
+
+        var requester = new CheckedBase<BuildType>(Specification.authSpec(user), Endpoint.BUILD_TYPES);
+        AtomicReference<String> buildTypeId = new AtomicReference<>("");
+
+        var buildType = generate(BuildType.class);
+        buildType.setProject(Project.builder().id(projectId.get()).locator(null).build());
+
+        step("Create buildType for project by user", () -> {
+            buildTypeId.set(requester.create(buildType).getId());
+        });
+
+        step("Check buildTYpe was created successfully with correct data", () -> {
+            var createdBuildType = requester.read(buildTypeId.get());
+            softy.assertEquals(buildType.getName(), createdBuildType.getName(), "Error");
+        });
     }
 
     @Test(description = "User should not be able to create build type with same id", groups = {"Negative", "CRUD"})
