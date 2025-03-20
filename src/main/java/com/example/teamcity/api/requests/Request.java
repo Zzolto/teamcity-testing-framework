8602ae397c8e61ad4cfdaffefcbc02b34e09193a
Zolto <zolto_gunzenov@mail.ru>
2025-03-20 13:58:56 +0300

    add checked and unchecked tests

--- src/main/java/com/example/teamcity/api/requests/Request.java
+++ src/main/java/com/example/teamcity/api/requests/Request.java
@@ -2,11 +2,13 @@ package com.example.teamcity.api.requests;
 
 import com.example.teamcity.api.enums.Endpoint;
 import io.restassured.specification.RequestSpecification;
+import lombok.Getter;
 
+@Getter
 public class Request {
 
-    private final RequestSpecification spec;
-    private final Endpoint endpoint;
+    protected final RequestSpecification spec;
+    protected final Endpoint endpoint;
 
     public Request(RequestSpecification spec, Endpoint endpoint) {
         this.spec = spec;
